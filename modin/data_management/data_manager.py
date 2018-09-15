d2651954b9333f4c5f32d14f2b39f7cd906a75d2
William Ma <wma1017@gmail.com>
2018-09-14 18:12:52 -0700

    Imported is_numeric_dtype from pandas

--- modin/data_management/data_manager.py
+++ modin/data_management/data_manager.py
@@ -7,7 +7,7 @@ import pandas
 
 from pandas.compat import string_types
 from pandas.core.dtypes.cast import find_common_type
-from pandas.core.dtypes.common import (_get_dtype_from_object, is_list_like)
+from pandas.core.dtypes.common import (_get_dtype_from_object, is_list_like, is_numeric_dtype)
 from pandas.core.index import _ensure_index
 
 from .partitioning.partition_collections import BlockPartitions, RayBlockPartitions
