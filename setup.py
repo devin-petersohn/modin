c5a3c10e90496511167f822f7a89926f10def8d8
Devin Petersohn <devin.petersohn@gmail.com>
2020-01-10 15:12:50 -0800

    Make work on cloud

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
 
 dask_deps = ["dask>=2.1.0", "distributed>=2.3.2"]
 ray_deps = ["ray==0.8.0"]
@@ -12,7 +12,7 @@ setup(
     description="Modin: Make your pandas code run faster by changing one line of code.",
     packages=find_packages(),
     url="https://github.com/modin-project/modin",
-    long_description=long_description,
+    # long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=["pandas==0.25.3"],
     extras_require={
