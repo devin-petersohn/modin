39cbcb5cc195fdd8dce273cb996ed9dda47a17e5
Devin Petersohn <devin.petersohn@gmail.com>
2020-01-10 14:31:50 -0800

    Fix kwargs wrapping

--- modin/engines/cloudburst/task_wrapper.py
+++ modin/engines/cloudburst/task_wrapper.py
@@ -8,8 +8,8 @@ if __execution_engine__ == "Cloudburst":
 class CloudburstTask:
     @classmethod
     def deploy(cls, func, num_return_vals, kwargs):
-        f = droplet.register(lambda _, **kwargs: func(**kwargs), func.__name__)
-        future_obj = f(**kwargs)
+        f = droplet.register(lambda _, kwargs: func(**kwargs), func.__name__)
+        future_obj = f(kwargs)
         unpack = droplet.register(lambda _, l, i: l[i], "unpack")
         return [unpack(future_obj, i) for i in range(num_return_vals)]
 
--- modin/engines/cloudburst/utils.py
+++ modin/engines/cloudburst/utils.py
@@ -7,7 +7,7 @@ droplet = None
 def get_or_init_client():
     global droplet
     if droplet is None:
-        ip = os.eviron.get("MODIN_IP", None)
+        ip = os.environ.get("MODIN_IP", None)
         conn = os.environ.get("MODIN_CONNECTION", None)
         droplet = DropletConnection(conn, ip)
     return droplet
