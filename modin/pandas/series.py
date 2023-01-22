be66ab6243abff0c494a0f36eef7ea48a2d229ae
Naren Krishna <naren@ponder.io>
2023-01-21 21:50:14 -0800

    Fix nlargest and smallest support
    Signed-off-by: Naren Krishna <naren@ponder.io>

--- modin/pandas/series.py
+++ modin/pandas/series.py
@@ -1242,13 +1242,9 @@ class Series(SeriesCompat, BasePandasDataset):
         """
         Return the largest `n` elements.
         """
-        if len(self._query_compiler.columns) == 0:
-            raise NotImplementedError(
-                "Series.nlargest is not implemented for empty Series."
-            )
-        return DataFrame(
+        return Series(
             query_compiler=self._query_compiler.nlargest(
-                n, [self._query_compiler.columns[0]], keep
+                n=n, columns=self.name, keep=keep
             )
         )
 
@@ -1256,13 +1252,9 @@ class Series(SeriesCompat, BasePandasDataset):
         """
         Return the smallest `n` elements.
         """
-        if len(self._query_compiler.columns) == 0:
-            raise NotImplementedError(
-                "Series.nlargest is not implemented for empty Series."
-            )
-        return DataFrame(
+        return Series(
             query_compiler=self._query_compiler.nsmallest(
-                n, [self._query_compiler.columns[0]], keep
+                n=n, columns=self.name, keep=keep
             )
         )
 
